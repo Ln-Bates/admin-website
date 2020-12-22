9f02072e98d4e0f69bfc2d86e4b9dc942e08613f
wbc <bates.wang@71360.com>
2020-12-22 21:02:37 +0800

    build: 添加别名

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
+//拼接路径
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
 module.exports = {
     css: {
         loaderOptions: {
@@ -5,5 +11,12 @@ module.exports = {
                 prependData: `@import "@/styles/_variable.scss";`
             }
         }
-    }
+    },
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('apis', resolve('src/apis'))
+            .set('components', resolve('src/components'))
+            .set('utils', resolve('src/utils'))
+    },
 }
